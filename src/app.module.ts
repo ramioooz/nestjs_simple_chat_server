a90a3d67b2e1caefa9829cb9536a3d28d06fde7f
Rami Mohamed <rami.workstation@gmail.com>
2024-04-29 02:01:12 +0400

    added support for .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [MessagesModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
