5795f94547a154fcce690b8cb786f1d034cab219
Rami Mohamed <rami.workstation@gmail.com>
2024-04-29 01:42:08 +0400

    adding process.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // await app.listen(3000);
+  const portNum = process.env.PORT;
+  console.log(`port number is: ${portNum}`);
+  await app.listen(portNum);
 }
 bootstrap();
